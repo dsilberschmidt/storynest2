f411c03bdfb363f30c8e5ffe6996dbd3c1a1b179
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2025-04-28 08:36:19 +0200

    added questions file

---
+++ src/app/questions.js
@@ -0,0 +1,10 @@
+
+const questions = [
+  "What is your earliest memory?",
+  "What was your childhood home like?",
+  "What games did you play when you were young?",
+  "Who was your best friend during your childhood?",
+  "What was your favorite food as a child?"
+];
+
+export default questions;
